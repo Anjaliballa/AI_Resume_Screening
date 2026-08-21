67fb2a7e39cb320db0ef7072744838c38321aa3b
Anjali <ballakamalaanjali@gmail.com>
2026-08-21 12:42:36 +0530

    Update test_report.py

--- test_report.py
+++ test_report.py
@@ -2,30 +2,18 @@ from parser.candidate_profile import create_candidate_profile
 from recommender.job_loader import load_jobs
 from recommender.recommendation import recommend_jobs
 from reports.report_generator import generate_report
-
-
 resume = "uploads/dresume.pdf"
-
-
 profile = create_candidate_profile(resume)
-
-
 jobs = load_jobs(
     "datasets/jobs.csv"
 )
-
-
 recommendations = recommend_jobs(
     profile,
     jobs
 )
-
-
 report = generate_report(
     profile,
     recommendations
 )
-
-
 print("Report Generated:")
 print(report)
