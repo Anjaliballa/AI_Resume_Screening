464619392fda833559253abd421dd6af35999fa7
Anjali <ballakamalaanjali@gmail.com>
2026-08-21 12:39:42 +0530

    Update test_preprocess.py

--- test_preprocess.py
+++ test_preprocess.py
@@ -1,10 +1,6 @@
 from parser.resume_parser import extract_resume_text
 from nlp.preprocess import preprocess_text
-
 file_path = "uploads/dresume.pdf"
-
 text = extract_resume_text(file_path)
-
 tokens = preprocess_text(text)
-
 print(tokens)
