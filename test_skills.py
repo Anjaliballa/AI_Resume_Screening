a92e2aee6d2bef21991404661e02c14d038668ce
Anjali <ballakamalaanjali@gmail.com>
2026-08-21 12:44:13 +0530

    Update test_skills.py

--- test_skills.py
+++ test_skills.py
@@ -1,14 +1,8 @@
 from parser.resume_parser import extract_resume_text
 from nlp.skill_extractor import extract_skills
-
-
 file_path = "uploads/dresume.pdf"
-
 text = extract_resume_text(file_path)
-
 skills = extract_skills(text)
-
 print("Skills Found:")
-
 for skill in skills:
     print("-", skill)
