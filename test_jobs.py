33e314f5f4a7537a0547dca453ef6d90810b19c1
Anjali <ballakamalaanjali@gmail.com>
2026-08-21 12:38:09 +0530

    Update test_jobs.py

--- test_jobs.py
+++ test_jobs.py
@@ -1,12 +1,6 @@
 from recommender.job_loader import load_jobs
-
-
 file_path = "datasets/jobs.csv"
-
 jobs = load_jobs(file_path)
-
 print("===== JOB DATASET =====")
-
 print(jobs)
-
 print("\nNumber of jobs:", len(jobs))
