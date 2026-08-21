54847fedce033e37197cb0de99100bc900c9ce82
Anjali <ballakamalaanjali@gmail.com>
2026-08-21 12:35:56 +0530

    Update test_education.py

--- test_education.py
+++ test_education.py
@@ -1,14 +1,8 @@
 from parser.resume_parser import extract_resume_text
 from parser.info_extractor import extract_education
-
-
 file_path = "uploads/dresume.pdf"
-
 text = extract_resume_text(file_path)
-
 education = extract_education(text)
-
 print("Education Found:")
-
 for item in education:
     print("-", item)
